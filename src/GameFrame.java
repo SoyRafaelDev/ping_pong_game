96b5411cfada11463d0048102cc76a105de1e9f6
Rafael Gonzalez <130540324+SoyRafaelDev@users.noreply.github.com>
2024-08-07 20:35:46 +0200

    first commit

---
+++ src/GameFrame.java
@@ -0,0 +1,34 @@
+import java.awt.*;
+import java.awt.event.*;
+import java.util.*;
+import javax.swing.*;
+
+public class GameFrame {
+    GameFrame() {
+
+        public void newBall(){
+
+        }
+        public void newPaddel(){
+
+        }
+        public void paint(Graphics g){
+
+        }
+        public void paint(Graphics g){
+
+        }
+        public void move(){
+
+        }
+        public void checkCollision(){
+
+        }
+        public void run(){
+
+        }
+
+
+
+    }
+}
